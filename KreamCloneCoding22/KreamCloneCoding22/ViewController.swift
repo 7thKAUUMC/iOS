6dfb7f30cb70875baa56d692fdccf381d5cee2e1
SOOHYUNLEE08 <lsoo0827@gmail.com>
2024-10-18 13:51:39 +0900

    First Commit

---
+++ KreamCloneCoding22/KreamCloneCoding22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KreamCloneCoding22
+//
+//  Created by 이수현 on 10/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
